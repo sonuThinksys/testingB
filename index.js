a2fb69c3ae53b750e5b899fe7ebfa95b4e1066e4
navnits <patel.sonu@thinksys.com>
2025-12-29 15:59:12 +0530

    change in db

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
 // Body parsers
+const initDB = require("./controllers/init");
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
-
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await initDB();
   console.log(`Server running at http://localhost:${PORT}`);
 });
 
